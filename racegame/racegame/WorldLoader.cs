7a4561f8b4ab5d52323ef19fdbda25da6e1725a3
Remco <remcodebruin@gmx.net>
2012-10-11 00:13:58 +0200

    rond kloten

---
+++ racegame/racegame/WorldLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace racegame
+{
+    class tile {
+        
+    }
+    class WorldLoader {
+        public tile[][] world;
+
+        public void load(string image) {
+            Texture2D.FromStream
+            for(x
+                for(y
+                    tile[x][y] = img.
+                        //nvm :D
+        }
+          
+          
+          
+          
+        
+    }
+}
