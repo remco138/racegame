c3f2417fa360f1d4f6e93d60238845f4495f9fd1
Remco <remcodebruin@gmx.net>
2012-10-30 22:11:56 +0100

    nieuwe branch test

---
+++ racegame/racegame/Hud/Hud.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace racegame
+{
+    class Hud
+    {
+        private SpriteFont font;
+
+        public Hud(ContentManager content)
+        {
+            font = content.Load<SpriteFont>("Fonts/Pericles Light");
+        }
+        public void draw(SpriteBatch spriteBatch, Track track)//List<Car> cars)
+        {
+            //draw awesome art here
+
+            //for (int i = 0; i < cars.Count(); i++)
+            //{
+            track.
+                spriteBatch.DrawString(font, Convert.ToString(320), new Vector2(10, 10), Color.BlanchedAlmond);
+            //}
+        }
+    }
+}
